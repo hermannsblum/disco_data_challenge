df4d2153382e7d3e7b4957f0958272073e4cc533
hermannsblum <hermannsblum@yahoo.de>
2015-04-24 22:22:20 +0200

    data can now be imported for each data-file individually

--- import_data.py
+++ import_data.py
@@ -2,17 +2,49 @@ import pandas as pd
 import json
 
 
-def import_businesses():
-    businesses_file = 'data/yelp_academic_dataset_business.json'
+DATA_PATH = 'data'
 
-    businesses_dict = []
-    with open(businesses_file) as file:
-        for line in file:
-            businesses_dict.append(json.loads(line))
+BUSINESSES_PATH = DATA_PATH + '/yelp_academic_dataset_business.json'
+CHECKINS_PATH = DATA_PATH + '/yelp_academic_dataset_checkin.json'
+REVIEWS_PATH = DATA_PATH + '/yelp_academic_dataset_review.json'
+TIPS_PATH = DATA_PATH + '/yelp_academic_dataset_tip.json'
+USERS_PATH = DATA_PATH + '/yelp_academic_dataset_user.json'
 
-    businesses = pd.DataFrame(businesses_dict)
-    return businesses
 
-if __name__ == '__main__':
-    businesses = import_businesses()
-    print(list(businesses.columns.values))
+def import_file(path):
+    def decorate(func):
+        def decorated():
+            data = []
+            with open(path) as file:
+                for line in file:
+                    data.append(json.loads(line))
+            ret = pd.DataFrame(data)
+            func(ret.columns.values)
+            return ret
+        return decorated
+    return decorate
+
+
+@import_file(BUSINESSES_PATH)
+def import_businesses(columns):
+    print('Successfully imported businesses with columns %s' % columns)
+
+
+@import_file(CHECKINS_PATH)
+def import_checkins(columns):
+    print('Successfully imported checkins with columns %s' % columns)
+
+
+@import_file(REVIEWS_PATH)
+def import_reviews(columns):
+    print('Successfully imported reviews with columns %s' % columns)
+
+
+@import_file(TIPS_PATH)
+def import_tips(columns):
+    print('Successfully imported tips with columns %s' % columns)
+
+
+@import_file(USERS_PATH)
+def import_users(columns):
+    print('Successfully imported users with columns %s' % columns)
