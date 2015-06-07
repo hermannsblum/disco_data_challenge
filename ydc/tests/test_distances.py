82ff32aa09e63e255bf94793ae2aa033be3a98f3
hermannsblum <hermannsblum@yahoo.de>
2015-06-07 13:02:58 +0200

    implementation of new distance-calculation

--- ydc/tests/test_distances.py
+++ ydc/tests/test_distances.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from ydc.tools import distances, import_data
+# from ydc.tools import distances_memcells
+# import time
 
 
 class TestDistances(TestCase):
@@ -9,9 +11,104 @@ class TestDistances(TestCase):
         self.businesses = import_data.import_businesses()[:20]
 
     def test_neighbours(self):
-        neirest = distances.neirest_neighbour(self.businesses[1:2].squeeze(),
-                                              self.businesses)
+        # create cell collection
+        cells = distances.CellCollection(15, self.businesses)
 
-        small_region = distances.get_neighbours(self.businesses[1:2].squeeze(),
-                                                self.businesses)
-        self.assertTrue(neirest.business_id in small_region['business_id'].tolist())
+        neirest = cells.get_neighbours(self.businesses[1:2].squeeze(), num=1)
+        neirest = neirest[0]
+        self.assertTrue('distance' in neirest.keys())
+
+        small_region = cells.get_neighbours(self.businesses[1:2].squeeze(),
+                                            num=10)
+        indizes = []
+        for item in small_region:
+            indizes.append(item['index'])
+        self.assertTrue(neirest['index'] in indizes)
+
+        # cells = distances_memcells.CellCollection(80000, 40000,
+        #                                           self.businesses)
+        # cells.get_neighbours(self.businesses[1:2].squeeze(), self.businesses)
+
+    def test_performance(self):
+
+        """
+        print("Testing the Filterung")
+        print("filter")
+        start = time.clock()
+        for i in range(100):
+            left = self.businesses[self.businesses.longitude < 1.2]
+            right = self.businesses[self.businesses.longitude >= 1.2]
+        end = time.clock()
+        print((end-start)/100)
+
+        print("notin")
+        start = time.clock()
+        for i in range(100):
+            left = self.businesses[self.businesses.longitude < 1.2]
+            right = self.businesses[~self.businesses.isin(left)]
+        end = time.clock()
+        print((end-start)/100)
+
+        print("apply")
+        start = time.clock()
+        for i in range(100):
+            left = self.businesses.apply(lambda row: row.longitude < 1.2,
+                                         axis=1)
+            one = self.businesses[left == 1]
+            two = self.businesses[left == 0]
+        end = time.clock()
+        print((end-start)/100)
+
+        print("query")
+        start = time.clock()
+        for i in range(100):
+            left = self.businesses.query("longitude < {}".format(1.2))
+            right = self.businesses.query("longitude >= {}".format(1.2))
+        end = time.clock()
+        print((end-start)/100)
+
+        self.assertTrue(False)
+        """
+        """
+        print("Creating CellCollection")
+        start = time.clock()
+        cells = distances.CellCollection(15, self.businesses)
+        end = time.clock()
+        print((end - start))
+
+        print("Find Neighbours in CellCollection")
+        start = time.clock()
+        for i in range(10):
+            cells.get_neighbours(self.businesses[1:2].squeeze())
+        end = time.clock()
+        print((end - start) / 10)
+
+        print("Find Neighbours in whole DataFrame")
+        start = time.clock()
+        for i in range(10):
+            distances.get_neighbours(self.businesses[1:2].squeeze(),
+                                     self.businesses)
+        end = time.clock()
+        print((end - start) / 10)
+
+        self.assertTrue(False)
+        """
+"""
+
+    def test_recursive_cells(self):
+
+        Test the recursive implementation
+        start = time.clock()
+        cells = distances_memcells.Cell(-180, 180, -90, 90, 5)
+        for idx, business in self.businesses.iterrows():
+            cells.append(business.to_dict())
+        end = time.clock()
+        print(end-start)
+
+        start = time.clock()
+        cells = distances_memcells.CellCollection(15, self.businesses)
+        end = time.clock()
+        print((end-start))
+        self.assertTrue(False)
+
+"""
--- ydc/tools/distances.py
+++ ydc/tools/distances.py
@@ -6,94 +6,128 @@ import pandas as pd
 
 class CellCollection:
 
-    def __init__(self, num_x, num_y):
-        self.longitudes = linspace(-180, 180, num_x)
-        self.latitudes = linspace(-90, 90, num_y)
+    def __init__(self, potenz, businesses):
+        """Create CellCollection
+        :param potenz: The CellCollection has dimension 2^(potenz+1) x 2^potenz
+        :param businesses: The businesses stored in these Cells
+        :returns: CellCollection"""
+
+        def fill_cells(self, latitudes, longitudes, businesses):
+            """Sort the businesses into the cells recursively
+            :param latitudes: the nparray of longitudes in the cell-grid
+            :param latitudes: the nparray of latitudes in the cell-grid
+            :param businesses: the DataFrame of businesses to sort into this
+                CellCollection
+            :returns: The CellCollection as dict of dicts. Keys are the inizes
+                in the longitudes and latitudes arrays of the lower left point
+                of a cell"""
+            if len(businesses) == 0:
+                # There are no businesses in this area
+                return
+            if longitudes.size > 2:
+                # First we devide the collection along the center of longitudes
+                center = longitudes[len(longitudes) / 2]
+                fill_cells(self, latitudes,
+                           longitudes[:(longitudes.size / 2 + 1)],
+                           businesses[businesses.longitude <= center])
+                fill_cells(self, latitudes, longitudes[(longitudes.size / 2):],
+                           businesses[businesses.longitude > center])
+            elif latitudes.size > 2:
+                # Now we devide along the center of latitudes
+                center = latitudes[latitudes.size / 2]
+                fill_cells(self, latitudes[:(latitudes.size / 2 + 1)],
+                           longitudes,
+                           businesses[businesses.latitude <= center])
+                fill_cells(self, latitudes[(latitudes.size / 2):], longitudes,
+                           businesses[businesses.latitude > center])
+            else:
+                # Now we are in one Cell! Let's store our businesses
+                bizs = []
+                for idx, business in businesses.iterrows():
+                    # For performance, we only store index and coordinates
+                    bizs.append({'index': idx,
+                                 'latitude': business.latitude,
+                                 'longitude': business.longitude})
+                coord = (self.longitudes.searchsorted(longitudes[0]),
+                         self.latitudes.searchsorted(latitudes[0]))
+                if coord[0] not in self.cells:
+                    self.cells[coord[0]] = {}
+                if len(bizs) > 0:
+                    # We don't need empty entries
+                    self.cells[coord[0]].update({coord[1]: bizs})
+
+        self.longitudes = linspace(-180, 180, (2 ** (potenz + 1)) + 1)
+        self.latitudes = linspace(-90, 90, (2 ** potenz) + 1)
+
+        # fill the cells of our new array
+        self.cells = {}
+        fill_cells(self, self.latitudes, self.longitudes, businesses)
 
     def get_cell(self, business):
-        x = 0
-        while x < self.longitudes.size - 1:
-            if business.longitude > self.longitudes[x] and \
-                    business.longitude < self.longitudes[x + 1]:
-                break
-            else:
-                x = x + 1
-        y = 0
-        while y < self.latitudes.size - 1:
-            if business.latitude > self.latitudes[y] and \
-                    business.latitude < self.latitudes[y + 1]:
-                break
-            else:
-                y = y + 1
+        """Finds the Cell the given businesses would belong to
+        :returns: Coordinates as a Tuple"""
+        x = self.longitudes.searchsorted(business.longitude)
+        y = self.latitudes.searchsorted(business.latitude)
         return x, y
 
+    def get_neighbours(self, business, num=5):
+        """Finds neighbours of a given business
+        :param business: the business we search neighbours for
+        :param num: minimum-number of neighbours returnes, default 5
+        :returns: list of dicts with 'index', 'latitude', 'longitude',
+            'distance'"""
 
-class ExponentialRadius:
+        def radius_step(radius, num_longtidues, num_latitudes, time):
+            """expand the search-radius exponentially"""
+            step = int(exp(time))
+            radius['long_down'] = radius['long_down'] - step
+            if radius['long_down'] <= 0:
+                radius['long_down'] = 0
+            radius['long_up'] = radius['long_up'] + step
+            if radius['long_up'] >= num_longtidues - 1:
+                radius['long_up'] = num_longtidues - 1
+            radius['lat_down'] = radius['lat_down'] - step
+            if radius['lat_down'] <= 0:
+                radius['lat_down'] = 0
+            radius['lat_up'] = radius['lat_up'] + step
+            if radius['lat_up'] >= num_latitudes - 1:
+                radius['lat_up'] = num_latitudes - 1
 
-    def __init__(self, step, business):
-        self.i = -1
-        self.long_point = float(business.longitude)
-        self.lat_point = float(business.latitude)
-        self.long_down = float(business.longitude)
-        self.long_up = float(business.longitude)
-        self.lat_down = float(business.latitude)
-        self.lat_up = float(business.latitude)
-        self.step_dist = float(step)
-
-    def step(self):
-        self.i = self.i + 1
-        step = self.step_dist * exp(self.i)
-
-        self.long_down = self.long_point - step
-        if self.long_down < -180:
-            self.long_down = -180
-        self.long_up = self.long_point + step
-        if self.long_up > 180:
-            self.long_up = 180
-        self.lat_down = self.lat_point - step
-        if self.lat_down < -90:
-            self.lat_down = -90
-        self.lat_up = self.lat_point + step
-        if self.lat_up > 90:
-            self.lat_up = 90
-
-    def inner_radius(self):
-        return vincenty((self.long_point, self.lat_point), (self.long_down, self.lat_point)).km
-
-
-def get_neighbours(business, businesses, num=5, step=0.1):
-    """
-    :param business: The business we search the neirest neighbour for
-    :param businesses: The DataFrame of businesses we search in
-    :param num: minimal number of neighbours to return
-    :param step: distance for one search step in km
-    """
-    # degrees for 1km at the aquator
-    onekm = 360 / 40074
-    # convert step to degrees
-    step = step * onekm
-    radius = ExponentialRadius(step, business)
-    neighbours = []
-    region = []
-    i = 0
-    while len(neighbours) < num and radius.inner_radius() < 100:
-        i = i + 1
-        while len(region) < num * exp(i):
-            print(radius.inner_radius())
-            radius.step()
-            query = ('index != "{0}" and longitude > {1} '
-                     "and longitude < {2} and latitude > {3} "
-                     "and latitude < {4}").format(
-                business.index, radius.long_down, radius.long_up,
-                radius.lat_down, radius.lat_up)
-            region = businesses.query(query)
-
-        print(radius.inner_radius())
-        neighbours = region
-        neighbours['distance'] = pd.Series(neighbours.apply(
-            lambda row: distance(business, row), axis=1), index=neighbours.index)
-        neighbours = neighbours[neighbours['distance'] < radius.inner_radius()]
-    return neighbours
+        cell = self.get_cell(business)
+        radius = {'long_down': cell[0], 'long_up': cell[0] + 1,
+                  'lat_down': cell[1], 'lat_up': cell[1] + 1}
+        ret = []
+        time = 0
+        inner_radius = 0
+        while len(ret) < num and inner_radius < 100:
+            ret = []
+            found = []
+            radius_step(radius, self.longitudes.size, self.latitudes.size,
+                        time)
+            for row in range(radius['long_down'], radius['long_up']):
+                for col in range(radius['lat_down'], radius['lat_up']):
+                    if row in self.cells and col in self.cells[row]:
+                        for item in self.cells[row][col]:
+                            found.append(item)
+            # We approximate the in-radius of the search-rectangle by half of
+            # the distance between the centers of left and right border
+            # (Not exactly the in-radius on the surface of a sphereoid, but
+            # easier to calculate)
+            inner_radius = vincenty((self.longitudes[radius['long_down']],
+                                     self.latitudes[cell[1]]),
+                                    (self.longitudes[radius['long_up']],
+                                     self.latitudes[cell[1]])).km / 2
+            for neighbour in found:
+                dist = vincenty((business.longitude, business.latitude),
+                                (neighbour['longitude'], neighbour['latitude'])
+                                ).km
+                # make sure we only include businesses in the in-circle of the
+                # search-rectangle
+                if dist <= inner_radius and \
+                        neighbour['index'] != business.name:
+                    neighbour['distance'] = dist
+                    ret.append(neighbour)
+        return ret
 
 
 def distance(a, b):
@@ -105,11 +139,12 @@ def distance(a, b):
                     (float(b.longitude), float(b.latitude))).km
 
 
-def neirest_neighbour(business, businesses):
+def neirest_neighbour(business, cells):
     """Find neirest neighbour of a business
     :param business: The business we search the neirest neighbour for
     :param businesses: The DataFrame of businesses we search in
     :returns: neighbour id and distance"""
-    neighbours = get_neighbours(business, businesses, num=1)
+    array = cells.get_neighbours(business, num=1)
+    neighbours = pd.DataFrame(array).set_index('index')
     index = neighbours['distance'].idxmin()
     return neighbours.loc[index]
